1c5d6bbf588b81ee3cb546d7d7d6a159c3373102
uehara yuta <octaviusyuta86@gmail.com>
2020-12-27 14:40:04 +0900

    Initial Commit

---
+++ aldiam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aldiam
+//
+//  Created by Owner on 2020/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
