c4ca48c27b6bb5445098eaf5c864f5ff96bef066
uehara yuta <octaviusyuta86@gmail.com>
2020-12-28 16:21:25 +0900

    init commit

--- aldiam/ViewController.swift
+++ aldiam/ViewController.swift
@@ -7,16 +7,22 @@
 
 import UIKit
 import FSCalendar
+import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FSCalendarDelegate {
 
     @IBOutlet weak var calendar: FSCalendar!
     
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var memoTextView: UITextView!
+    
+    var selectedDate: Date = Date()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        calendar.delegate = self
         
         self.calendar.calendarWeekdayView.weekdayLabels[0].text = "日"
         self.calendar.calendarWeekdayView.weekdayLabels[1].text = "月"
@@ -27,6 +33,50 @@ class ViewController: UIViewController {
         self.calendar.calendarWeekdayView.weekdayLabels[6].text = "土"
         
         
+    }
+    
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        selectedDate = date
+        displayDiary()
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        do {
+            let realm = try Realm()
+            let diary = diaryModel()
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy.MM.dd"
+
+
+            let dateString = dateFormatter.string(from: selectedDate)
+            diary.title = titleTextField.text ?? ""
+            diary.memo = memoTextView.text
+            diary.date = dateString
+            
+            
+            try realm.write {
+                realm.add(diary)
+            }
+        } catch {
+            print("create todo error.")
+        }
+    }
+    
+    func displayDiary(){
+        let realm = try! Realm()
+        let diaries = realm.objects(diaryModel.self)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy.MM.dd"
+
+
+        let dateString = dateFormatter.string(from: selectedDate)
+        for diary in diaries {
+            if diary.date == dateString{
+                titleTextField.text = diary.title
+                memoTextView.text = diary.memo
+                return
+            }
+        }
     }
 
 
---
+++ aldiam/diary.swift
@@ -0,0 +1,17 @@
+//
+//  diary.swift
+//  aldiam
+//
+//  Created by Owner on 2020/12/28.
+//
+
+import Foundation
+import RealmSwift
+
+class diaryModel: Object {
+    @objc dynamic var title = ""
+    @objc dynamic var memo = ""
+    @objc dynamic var date = "" //yyyy.MM.dd
+}
+
+
